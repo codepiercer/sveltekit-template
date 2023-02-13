9b4097886ebaab8c400bf83e044782d585697fa2
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2023-02-13 13:26:26 -0500

    Add dynamic server port option from environment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ const config = {
   define: {
     'import.meta.env.BUILD_TIME': JSON.stringify(new Date().toISOString()),
     'import.meta.env.BUILD_VERSION': JSON.stringify(process.env.npm_package_version)
+  },
+  server: {
+    port: process.env.PORT
   }
 }
 
