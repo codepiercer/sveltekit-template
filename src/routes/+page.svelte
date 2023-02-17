d3360a2000ceaf232a2962315428b0ddd841cff3
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2023-02-16 23:02:14 -0500

    Update dependencies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import { Button } from '@codepiercer/svelte-tailwind'
 </script>
 
-<div class="prose flex h-screen max-w-none flex-col items-center justify-center">
+<div class="flex h-screen max-w-none flex-col items-center justify-center">
   <h1>Welcome to SvelteKit</h1>
   <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
   <Button>Click me</Button>
