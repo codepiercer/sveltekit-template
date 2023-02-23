e16877019b063dd8ea08737f6fcb8d803caee196
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2023-02-23 01:06:58 -0500

    Update dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,5 +11,9 @@ module.exports = {
     browser: true,
     es2017: true,
     node: true
+  },
+  rules: {
+    'object-shorthand': ['error', 'always'],
+    'prefer-const': 'error'
   }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,21 @@
 <script>
-  import { Button } from '@codepiercer/svelte-tailwind'
+  import { Button, Dialog } from '@codepiercer/svelte-tailwind'
+
+  let dialog
 </script>
 
-<div class="flex h-screen max-w-none flex-col items-center justify-center">
+<div class="flex h-screen w-full max-w-none flex-col items-center justify-center gap-2">
   <h1>Welcome to SvelteKit</h1>
   <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <Button>Click me</Button>
+  <Button on:click={dialog.show}>Do not click me!</Button>
 </div>
+
+<Dialog bind:dialog closeOnOverlayClick>
+  <div slot="content">
+    <img
+      src="https://media.giphy.com/media/lgcUUCXgC8mEo/giphy.gif"
+      alt="rick roll"
+      class="mx-auto"
+    />
+  </div>
+</Dialog>
